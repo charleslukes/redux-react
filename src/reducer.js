8f3fdf3f2df229471297effa7c91a26fedcdb52b
Charles Chiakwa <charleslukes28@gmail.com>
2019-08-07 15:00:02 +0100

    add the reducer to the redux app

---
+++ src/reducer.js
@@ -0,0 +1,13 @@
+import { ADD_ARTICLE } from "./action/constants";
+
+const initialState = {
+  articles: [],
+}
+
+export const rootReducer = (state = initialState, action) => {
+  if(action.type === ADD_ARTICLE){
+    Object.assign({}, state, {
+      articles: state.articles.concat(action.payload)
+    })
+  }
+}
