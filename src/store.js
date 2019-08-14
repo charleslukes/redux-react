950c867502983431258fd38a2ffb738d4b2f6593
Charles Chiakwa <charleslukes28@gmail.com>
2019-08-14 20:05:48 +0100

    remnoved redux thunk and use redux saga

--- src/store.js
+++ src/store.js
@@ -1,10 +1,16 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { rootReducer } from './reducer';
 import {forbidden} from './middlewares';
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import apiSaga from './sagas/index';
+
+const initializeSagaMiddleware = createSagaMiddleware();
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export const store = createStore(rootReducer, 
-  storeEnhancers(applyMiddleware(forbidden, thunk))
+  storeEnhancers(applyMiddleware(forbidden, initializeSagaMiddleware))
 );
+
+initializeSagaMiddleware.run(apiSaga);
