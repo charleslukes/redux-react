f908924ecb15dcc44a6c04d74dba386041519b46
Charles Chiakwa <charleslukes28@gmail.com>
2019-08-14 19:32:30 +0100

    add the middlewares

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,4 +1,5 @@
 import {ADD_ARTICLE} from '../action/constants';
+import {DATA_LOADED} from '../action/constants';
 
 const forbiddenWords = ['spam', 'money'];
 
@@ -7,13 +8,26 @@ export const forbidden = ({dispatch}) => {
     return (action) => {
       if (action.type === ADD_ARTICLE) {
         const foundWord = forbiddenWords.filter(word => {
-          return action.payload.title.includes(word);
+          return action.payload.value.includes(word);
         })
-        if (foundWord) {
-          return dispatch({ type: 'FOUND BAD WORD'})
+        if (foundWord.length) {
+          return dispatch({ type: 'FOUND_BAD_WORD'})
         }
       }
       return next(action);
+    }
+  }
+}
+
+
+export const getData =  () => {
+  return async(dispatch) => {
+    try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const json = await res.json();
+    return dispatch({type: DATA_LOADED, payload: json});
+    } catch (error) {
+      return dispatch({type: DATA_LOADED, payload: 'Error'})
     }
   }
 }
