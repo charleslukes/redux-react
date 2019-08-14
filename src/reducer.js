239c4e29a839ee4dd71c7fcf994b5b5f35d3df63
Charles Chiakwa <charleslukes28@gmail.com>
2019-08-14 19:33:47 +0100

    updated the reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,10 @@
 import { ADD_ARTICLE } from "./action/constants";
+import { FOUND_BAD_WORD } from "./action/constants";
+import { DATA_LOADED } from "./action/constants";
 
 const initialState = {
   articles: [],
+  remoteArticles: []
 }
 
 export const rootReducer = (state = initialState, action) => {
@@ -10,6 +13,17 @@ export const rootReducer = (state = initialState, action) => {
       articles: state.articles.concat(action.payload)
     })
   }
+  if(action.type === FOUND_BAD_WORD){
+    return Object.assign({}, state, {
+      articles: state.articles.concat({value: 'bad word'})
+    })
+  }
+  if (action.type === DATA_LOADED) {
+    return Object.assign({}, state, {
+      remoteArticles: state.remoteArticles.concat(action.payload)
+    });
+  }
+
   return state;
 }
 
