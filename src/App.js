6e0eaf56a7193e5cf430851b30676324a6a32970
Charles Chiakwa <charleslukes28@gmail.com>
2019-08-14 19:34:58 +0100

    app can now display fetched data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import List from './components/List';
 import Form from './components/Form';
+import Post from './components/Posts';
 
 function App() {
   return (
@@ -14,6 +15,10 @@ function App() {
         <h2>Add a new article</h2>
          <Form />
       </div>
+      <div className="col-md-4 offset-md-1">
+      <h2>API posts</h2>
+       <Post />
+    </div>
     </div>
   );
 }
